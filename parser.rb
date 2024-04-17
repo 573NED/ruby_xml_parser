a4ea604d4d46267e7567fd98cb326d5d3f7446e7
573ned <573ned@gmail.com>
2024-04-17 16:24:40 +0300

    add countries parser

--- parser.rb
+++ parser.rb
@@ -1,8 +1,11 @@
 dm = 'data_model.csv'
 regions_file = 'regions.csv'
 documents_file = 'documents.csv'
+countries_file = 'countries.csv'
 
 def save_data(file_path, data_array)
+  data_array = data_array[1..-1]
+
   File.open(file_path, 'w') do |file|
     data_array.each do |data|
       file.puts data
@@ -13,6 +16,7 @@ end
 File.open(dm, 'r') do |file|
   regions = []
   documents = []
+  countries = []
 
   file.each_line do |line|
     data = line.split(',')
@@ -22,9 +26,12 @@ File.open(dm, 'r') do |file|
       regions << "#{data[3][9..10]};#{data[4]};#{data[3][4..-1]}"
     when "Код документа удостоверяющего личность"
       documents << "#{data[4]};#{data[3]}"
+    when "[Справочник стран]"
+      countries << "#{data[4]};#{data[3][13..-1].split('Member').first};#{data[3]}"
     end
   end
 
   save_data(regions_file, regions)
   save_data(documents_file, documents)
+  save_data(countries_file, countries)
 end
