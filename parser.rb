135aa5ae7ccd46371d9bd2581c68da9937b966cd
573ned <573ned@gmail.com>
2024-04-17 15:58:05 +0300

    add function for write files

--- parser.rb
+++ parser.rb
@@ -2,20 +2,29 @@ dm = 'data_model.csv'
 regions_file = 'regions.csv'
 documents_file = 'documents.csv'
 
-File.open(dm, 'r') do |file|
-  File.open(regions_file, 'w') do |region|
-    File.open(documents_file, 'w') do |document|
-
-      file.each_line do |line|
-        data = line.split(',')
-
-        case data[1]
-        when "Наименование по ОКАТО [List]"
-          region.puts "#{data[3][9..10]};#{data[4]};#{data[3][4..-1]}"
-        when "Код документа удостоверяющего личность"
-          document.puts "#{data[4]};#{data[3]}"
-        end
-      end
+def save_data(file_path, data_array)
+  File.open(file_path, 'w') do |file|
+    data_array.each do |data|
+      file.puts data
     end
   end
+end
+
+File.open(dm, 'r') do |file|
+  regions = []
+  documents = []
+
+  file.each_line do |line|
+    data = line.split(',')
+
+    case data[1]
+    when "Наименование по ОКАТО [List]"
+      regions << "#{data[3][9..10]};#{data[4]};#{data[3][4..-1]}"
+    when "Код документа удостоверяющего личность"
+      documents << "#{data[4]};#{data[3]}"
+    end
+  end
+
+  save_data(regions_file, regions)
+  save_data(documents_file, documents)
 end
