8fb615d45412acf993effe16b8d4f92cf39d9558
573ned <573ned@gmail.com>
2024-04-17 15:46:24 +0300

    realese documents parser

--- parser.rb
+++ parser.rb
@@ -1,13 +1,20 @@
 dm = 'data_model.csv'
-output_file = 'regions.csv'
+regions_file = 'regions.csv'
+documents_file = 'documents.csv'
 
 File.open(dm, 'r') do |file|
-  File.open(output_file, 'w') do |output|
-    file.each_line do |line|
-      data = line.split(',')
+  File.open(regions_file, 'w') do |region|
+    File.open(documents_file, 'w') do |document|
 
-      if data[1] == "Наименование по ОКАТО [List]"
-        output.puts "#{data[3][9..10]};#{data[4]};#{data[3][4..-1]}"
+      file.each_line do |line|
+        data = line.split(',')
+
+        case data[1]
+        when "Наименование по ОКАТО [List]"
+          region.puts "#{data[3][9..10]};#{data[4]};#{data[3][4..-1]}"
+        when "Код документа удостоверяющего личность"
+          document.puts "#{data[4]};#{data[3]}"
+        end
       end
     end
   end
